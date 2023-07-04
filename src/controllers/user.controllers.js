7baf74b9ab0b58d9f8ea1533024eb9f78fbc72f2
David Umoru <umorudavido@gmail.com>
2023-07-04 18:00:02 +0100

    Implement signup controller

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const signup = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+module.exports = { signup };
