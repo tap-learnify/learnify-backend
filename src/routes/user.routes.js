2c2bf10cfbec1da43c4b3155646609da9c737d3b
David Umoru <umorudavido@gmail.com>
2023-07-05 17:42:51 +0100

    Import authMiddleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/user.controllers');
-
+const authMiddleware = require('../middlewares/auth.middlewares');
 
 router.post('/signup', signup);
 router.post('/login', login);
