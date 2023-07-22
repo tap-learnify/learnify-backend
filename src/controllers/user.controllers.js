8005ce0c146f1dc4ed3e1960ad1a223b22a3d119
David Umoru <umorudavido@gmail.com>
2023-07-22 15:48:42 +0100

    Add forgot password

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -10,4 +10,9 @@ const login = async (req, res) => {
   res.status(data.statusCode).json(data)
 };
 
-module.exports = { signup, login };
+const forgotPassword = async (req, res) => {
+  const data = await userServices.forgotPassword(req.body);
+  res.status(data.statusCode).json(data)
+};
+
+module.exports = { signup, login, forgotPassword };
