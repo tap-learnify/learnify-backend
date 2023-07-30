42f53585e8df3ff4c7600b8d41144a77d47bbcf9
David Umoru <umorudavido@gmail.com>
2023-07-30 17:39:20 +0100

    Define routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const UserRouter = require('./routes/user.routes');
+
 const connectDB = require("./configs/database")
 
 const app = express();
@@ -12,7 +12,11 @@ app.use(bodyParser.json());
 // connecting to db here
 connectDB(process.env.MONGO_URI)
 
-app.use('/User', UserRouter);
+const userRouter = require('./routes/user.routes');
+const coursesRouter = require('./routes/courses.routes');
+
+app.use('/user', userRouter);
+app.use('/courses', coursesRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
