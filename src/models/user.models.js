f7b01604ba02aee89669252feb679e841a499864
David Umoru <umorudavido@gmail.com>
2023-07-31 17:42:46 +0100

    Add field to store resetPin

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  resetPin: {
+    type: String,
+    default: null,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
