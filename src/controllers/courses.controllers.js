c4f82707f99479d8efa8bfad75d0f8f359c891a9
David Umoru <umorudavido@gmail.com>
2023-08-04 17:45:59 +0100

    Add error handling for controllers

--- src/controllers/courses.controllers.js
+++ src/controllers/courses.controllers.js
@@ -1,13 +1,21 @@
 const courseServices = require("../services/courses.services");
 
 const getAllCourses = async (req, res) => {
-  const data = await courseServices.getAllCourses();
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await courseServices.getAllCourses();
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const searchCourses = async (req, res) => {
-  const data = await courseServices.searchCourse(req.query);
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await courseServices.searchCourse(req.query);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = { getAllCourses, searchCourses };
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,23 +1,39 @@
-const userServices = require('../services/user.services');
+const userServices = require("../services/user.services");
 
 const signup = async (req, res) => {
-  const data = await userServices.signup(req.body);
-  res.status(data.statusCode).json(data)
+  try {
+    const data = await userServices.signup(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const login = async (req, res) => {
-  const data = await userServices.login(req.body);
-  res.status(data.statusCode).json(data)
+  try {
+    const data = await userServices.login(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const forgotPassword = async (req, res) => {
-  const data = await userServices.forgotPassword(req.body);
-  res.status(data.statusCode).json(data)
+  try {
+    const data = await userServices.forgotPassword(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const resetPassword = async (req, res) => {
-  const data = await userServices.resetPassword(req.body);
-  res.status(data.statusCode).json(data)
+  try {
+    const data = await userServices.resetPassword(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = { signup, login, forgotPassword, resetPassword };
