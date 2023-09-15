5e1989714f34e6c00e3e499d38defd5d10bd3acd
David Umoru <umorudavido@gmail.com>
2023-09-16 00:14:46 +0100

    Import cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 
 const connectDB = require("./configs/database");
 
